f8a55a6c6129925b03fc34ec497094a724fa8f97
Angela Cartagena <angela@airtasker.com>
2015-04-17 12:21:44 +1000

    Fix #44 AutoLayout iOS7 Crash

--- GBInfiniteScrollView/GBInfiniteScrollView/GBInfiniteScrollView.m
+++ GBInfiniteScrollView/GBInfiniteScrollView/GBInfiniteScrollView.m
@@ -1374,6 +1374,8 @@ static CGFloat const GBAutoScrollDefaultInterval = 3.0f;
         [self recenterCurrentView];
         [self updateNumberOfPages];
     }
+    
+    [self layoutIfNeeded];
 }
 
 - (void)recenterContent
